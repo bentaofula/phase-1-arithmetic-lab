0d9752919e2302655b9002131b6d5908069b0225
Benta <bentaakitwi500@gmail.com>
2023-03-15 13:05:16 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-
 // Write your code here
+const num1 = 31, num2 = 2;
+const multiply = num1 * num2;
+multiply;
+let random;
+random = Math.floor(Math.random() * 10) + 1;
+random;
+const num3 = 52, num4 = 12;
+const mod = num3 % num4;
+mod;
+let max = Math.max(4, 20, 13, 9, 18);
+max;
